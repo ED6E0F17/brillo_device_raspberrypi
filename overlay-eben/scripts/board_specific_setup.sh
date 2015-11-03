7b91554d588b3a3e9bff23bd00c8064fb05f7ff1
hexameron <hexameron>
2015-11-03 14:14:17 +0000

    Merge RPI2.

--- overlay-eben/scripts/board_specific_setup.sh
+++ overlay-eben/scripts/board_specific_setup.sh
@@ -29,15 +29,14 @@ install_raspberrypi_bootloader() {
   sudo mount -o loop,offset=${efi_offset},sizelimit=${efi_size} "$1" \
     "${efi_dir}"
 
-  info "Installing firmware"
-  sudo cp "${ROOT}/firmware/rpi/"* "${efi_dir}/"
+  info "Installing firmware and overlays"
+  sudo cp -r "${ROOT}/firmware/rpi/"* "${efi_dir}/"
 
-  info "Creating boot configuration files"
+  info "Copying kernel and configuration files"
   sudo cp "${ROOT}/boot/cmdline.txt" "${efi_dir}/"
   sudo cp "${ROOT}/boot/config.txt" "${efi_dir}/"
-
-  info "Copying kernel.img"
-  sudo cp "${ROOT}/boot/kernel.img" "${efi_dir}/"
+  sudo cp "${ROOT}/boot/dtImage" "${efi_dir}/kernel.img"
+  sudo cp "${ROOT}/boot/bcm2709-rpi-2-b.dtb" "${efi_dir}/bcm2709-rpi-2-b.dtb"
 
   sudo umount "${efi_dir}"
   rmdir "${efi_dir}"
