883424bdedb9217edb56170bcc0ce3f756ee922e
hexameron <hexameron>
2015-11-22 13:09:46 +0000

    Resize partition table for dual use.

--- device_rpi2/eben/bsp/initnetwork.sh
+++ device_rpi2/eben/bsp/initnetwork.sh
@@ -1,8 +1,8 @@
 #!/system/bin/sh
 
 # Setup networking when boot starts
-ifconfig eth0 10.0.2.15 netmask 255.255.255.0 up
-route add default gw 10.0.2.2
+ifconfig eth0 192.168.1.246 netmask 255.255.255.0 up
+route add default gw 192.168.1.1
 # Open up port 5555 for adb
 iptables -I INPUT -p tcp --dport 5555 -j ACCEPT -w
 # Run dnsmasq so we can access hosts on the Internet. This is needed since
