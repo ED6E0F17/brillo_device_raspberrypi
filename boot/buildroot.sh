70ae7e572dec3c988b7b54b62844562e9473a64f
hexameron <hexameron>
2016-03-27 09:16:55 +0000

    Update buildroot script.

--- boot/buildroot.sh
+++ boot/buildroot.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-mkdir tmp
+mkdir -p tmp
+mkdir -p tmpmount
 
 cp kernel.img tmp
 cp ../ramdisk.img tmp
 cp cmdline.ram tmp
 cp config.ram tmp
-cp bcm2708* tmp
-FSIZE=`du tmp | cut -f 1`
-dd if=/dev/zero of=tmp.img bs=1KiB count=$(($FSIZE+30))
+cp bcm27* tmp
+cp -r overlays tmp
+FSIZE=$((`du tmp |tail -n 1| cut -f 1`))
+dd if=/dev/zero of=tmp.img bs=1024 count=$(($FSIZE+30))
 
 sudo mkfs.vfat tmp.img
-sudo mount -o loop tmp.img tmp
+sudo mount -o loop tmp.img tmpmount
 
-sudo cp kernel.img tmp
-sudo cp ../ramdisk.img tmp
-sudo cp config.ram tmp/config.txt
-sudo cp cmdline.ram tmp/cmdline.txt
-sudo cp bcm2708* tmp
+sudo cp -r tmp/* tmpmount
+sudo umount tmpmount
 
-sudo umount tmp
 mv tmp.img ../usbboot.img
 rm -rf tmp
+rm -rf tmpmount
