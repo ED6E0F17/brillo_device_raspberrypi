5a67d62146cfbcc11f819c6b4b46ee28829e7683
hexameron <hexameron>
2016-05-21 07:26:02 +0000

    Route all the things.

--- common/init.net-setup.sh
+++ common/init.net-setup.sh
@@ -2,11 +2,11 @@
 echo heartbeat > /sys/class/leds/led0/trigger
 
 # Testing only: Setup wired networking
-# rpi model A/Zero gadget mode. 
-ifconfig usb0 10.10.10.10 netmask 255.255.255.0 up && route add default gw 10.10.10.1 \
+# RPI Model A/Zero gadget mode:
+ifconfig usb0 10.10.10.10 netmask 255.255.255.0 up && ip route add default via 10.10.10.1 \
 && dnsmasq --no-hosts --listen-address=127.0.0.1 --no-resolv --server=10.10.10.1 --pid-file < /dev/null
-# rpi model B, 2B:
-ifconfig eth0 192.168.1.10 netmask 255.255.255.0 up && route add default gw 192.168.1.1 \
+# RPI Model B, 2B:
+ifconfig eth0 192.168.1.10 netmask 255.255.255.0 up && ip route add default via 192.168.1.1 \
 && dnsmasq --no-hosts --listen-address=127.0.0.1 --no-resolv --server=192.168.1.1 --pid-file < /dev/null
 
 # Shill enables dnsmasq so we can access hosts on the Internet.
