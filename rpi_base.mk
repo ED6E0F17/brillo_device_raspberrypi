1f99a980a66e9e286a51b4dd51e927ebb295616a
hexameron <hexameron>
2017-01-07 22:10:28 +0000

    Remove WiFi features.
    Brillo will not be updated to fix compile failures.

--- rpi_base.mk
+++ rpi_base.mk
@@ -20,8 +20,6 @@
 BRILLO := 1
 BRILLO_USE_DBUS := 1
 BRILLO_USE_OMAHA := 1
-BRILLO_USE_SHILL := 1
-BRILLO_USE_WEAVE := 1
 BRILLO_USE_BINDER := 1
 
 JAVA_NOT_REQUIRED := true
@@ -138,9 +136,6 @@ PRODUCT_PACKAGES += \
   dhcpcd-6.8.2 \
   dnsmasq \
   hostapd \
-  shill \
-  wifi_init \
-  wpa_supplicant \
 
 # Metrics daemons and crash reporter.
 PRODUCT_PACKAGES += \
@@ -151,27 +146,9 @@ PRODUCT_PACKAGES += \
 #  metricsd \
 
 # It only makes sense to include apmanager if WiFi is supported.
-WIFI_SUPPORTED := true
-WIFI_DRIVER_HAL_MODULE := wifi_driver.bcm43438
-WIFI_DRIVER_HAL_PERIPHERAL := bcm43438
-PRODUCT_PACKAGES += apmanager
-SHILL_USE_WIFI := true
+WIFI_SUPPORTED := false
+SHILL_USE_WIFI := false
 SHILL_USE_BINDER := false
-
-# RTL8192CU WIFI Firmware
-BRCM_WIFI_FW_SRC := device/rpi/wifi/firmware/brcm
-BRCM_WIFI_FW_DST := system/vendor/firmware/brcm
-RTL8192CU_WIFI_FW_SRC := device/rpi/wifi/firmware/rtlwifi
-RTL8192CU_WIFI_FW_DST := system/vendor/firmware/rtlwifi
-
-PRODUCT_COPY_FILES += \
-	$(BRCM_WIFI_FW_SRC)/brcmfmac43430-sdio.bin:$(BRCM_WIFI_FW_DST)/brcmfmac43430-sdio.bin  \
-	$(BRCM_WIFI_FW_SRC)/brcmfmac43430-sdio.txt:$(BRCM_WIFI_FW_DST)/brcmfmac43430-sdio.txt \
-	$(BRCM_WIFI_FW_SRC)/BCM43430A1.hcd:$(BRCM_WIFI_FW_DST)/BCM43430A1.hcd \
-        $(RTL8192CU_WIFI_FW_SRC)/rtl8192cufw.bin:$(RTL8192CU_WIFI_FW_DST)/rtl8192cufw.bin     \
-        $(RTL8192CU_WIFI_FW_SRC)/rtl8192cufw_A.bin:$(RTL8192CU_WIFI_FW_DST)/rtl8192cufw_A.bin \
-        $(RTL8192CU_WIFI_FW_SRC)/rtl8192cufw_B.bin:$(RTL8192CU_WIFI_FW_DST)/rtl8192cufw_B.bin \
-        $(RTL8192CU_WIFI_FW_SRC)/rtl8192cufw_TMSC.bin:$(RTL8192CU_WIFI_FW_DST)/rtl8192cufw_TMSC.bin
 
 PRODUCT_PACKAGES += \
   3rd-party-packages \
@@ -258,10 +235,7 @@ DHCPCD_USE_DBUS=yes
 
 # Wireless debugging.
 PRODUCT_PACKAGES += \
-  iw \
-  libnl \
   ping \
-  wpa_cli \
 
 #
 # Bluetooth.
@@ -282,7 +256,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
   device/rpi/common/dbus.conf:system/etc/dbus.conf \
   device/rpi/common/weaved.conf:system/etc/weaved/weaved.conf \
-  device/rpi/common/wpa_supplicant.conf:/system/lib/shill/shims/wpa_supplicant.conf \
   device/rpi/common/dhcpcd-6.8.2.conf:/system/etc/dhcpcd-6.8.2/dhcpcd.conf \
 
 BOARD_SEPOLICY_DIRS := $(BOARD_SEPOLICY_DIRS) device/rpi/sepolicy
