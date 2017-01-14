d81ad1f25069a49c5f8b49f0d547842299272ed1
hexameron <hexameron>
2017-01-14 22:45:03 +0000

    Update manifest.

--- boot/makeboot.sh
+++ boot/makeboot.sh
@@ -5,16 +5,16 @@ simg2img system.img system.ext
 mkdir -p rootmount
 resize2fs -M system.ext
 sudo mount -o loop system.ext rootmount
-sudo find rootmount |sudo cpio -o --'format=newc' |gzip >boot/ramdisk.img
+sudo find rootmount |sudo cpio -o --'format=newc' |gzip >system_ramdisk.img
 sudo umount rootmount
 
 #make boot partition
 sudo rm -f boot/overlays/.*tmp
 sudo rm -f boot/overlays/.*cmd
 FSIZE=$((`du boot |tail -n 1| cut -f 1`))
-dd if=/dev/zero of=bootimage.fat bs=1024 count=$(($FSIZE+30))
+dd if=/dev/zero of=bootimage.fat bs=1024 count=$(($FSIZE+40))
 
-sudo mkfs.vfat bootimage.fat
+sudo mkfs.vfat -F 16 -s 1 bootimage.fat
 sudo mount -o loop bootimage.fat rootmount
 sudo cp -r boot/* rootmount
 
