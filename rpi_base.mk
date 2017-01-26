0fc03dabf122720ed2b486f212178fb91c3a445d
hexameron <hexameron>
2017-01-26 22:29:54 +0000

    Remove some packages.

--- rpi_base.mk
+++ rpi_base.mk
@@ -61,7 +61,6 @@ PRODUCT_PACKAGES := \
   adbd \
   bootctl \
   firewalld \
-  healthd \
   init \
   init.environ.rc \
   init.rc \
@@ -75,10 +74,6 @@ PRODUCT_PACKAGES := \
   linker \
   logcat \
   logd \
-  nativeperms \
-  nativepowerman \
-  peripheralman \
-  power_example \
   reboot \
   rootdev \
   service \
@@ -87,10 +82,6 @@ PRODUCT_PACKAGES := \
   sh \
   toolbox \
   toybox \
-  update_engine \
-  update_engine_client \
-  weaved \
-  webservd \
 
 PRODUCT_PACKAGES_ENG += \
   brillo-update-payload-key \
@@ -118,14 +109,6 @@ PRODUCT_PACKAGES += \
   dbus-monitor \
   dbus-send \
 
-# Audio dependencies.
-PRODUCT_PACKAGES += \
-  libaudioroute \
-  libmedia \
-  libtinyalsa \
-  libtinycompress \
-  local_time.default \
-
 # Connectivity packages.
 PRODUCT_PACKAGES += \
   cacerts \
@@ -133,14 +116,6 @@ PRODUCT_PACKAGES += \
   dhcpcd \
   dhcpcd-6.8.2 \
   dnsmasq \
-
-# Metrics daemons and crash reporter.
-PRODUCT_PACKAGES += \
-  crash_reporter \
-
-#  libmetrics \
-#  metrics_client \
-#  metricsd \
 
 # It only makes sense to include apmanager if WiFi is supported.
 WIFI_SUPPORTED := false
@@ -169,16 +144,6 @@ PRODUCT_PACKAGES += \
   tlsdate-helper \
   tlsdated \
 
-# TPM packages.
-PRODUCT_PACKAGES += \
-  libtrunks \
-  trunks_client \
-  trunksd \
-  libtpm2\
-  libtpm_manager \
-  tpm_manager_client \
-  tpm_managerd \
-
 # This configures filesystem capabilities.
 TARGET_ANDROID_FILESYSTEM_CONFIG_H := \
 device/rpi/common/android_filesystem_config.h
@@ -199,32 +164,6 @@ AB_OTA_PARTITIONS := \
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_HOSTAPD_DRIVER := NL80211
-
-# Enable WPA CLI support.
-CONFIG_CTRL_IFACE=y
-# Enable WPA supplicant D-Bus support.
-CONFIG_CTRL_IFACE_DBUS=y
-CONFIG_CTRL_IFACE_DBUS_NEW=y
-
-# Enable disable_vht option in WPA supplicant.
-CONFIG_VHT_OVERRIDES=y
-
-# WPA supplicant basic authentication methods.
-CONFIG_DYNAMIC_EAP_METHODS=y
-CONFIG_IEEE8021X_EAPOL=y
-CONFIG_EAP_MD5=y
-CONFIG_EAP_MSCHAPV2=y
-CONFIG_EAP_TLS=y
-CONFIG_EAP_PEAP=y
-CONFIG_EAP_TTLS=y
-CONFIG_EAP_GTC=y
-CONFIG_EAP_OTP=y
-CONFIG_EAP_LEAP=y
-CONFIG_PKCS12=y
-CONFIG_PEERKEY=y
-CONFIG_BGSCAN_SIMPLE=y
-CONFIG_BGSCAN_LEARN=y
-CONFIG_IEEE80211W=y
 
 # Settings for dhcpcd-6.8.2.
 DHCPCD_USE_IPV6=yes
