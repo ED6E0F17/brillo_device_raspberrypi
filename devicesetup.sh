1a4ab1b49739434977293f79e9babdae9d4c6161
hexameron <hexameron>
2017-01-31 12:42:58 +0000

    Less paranoid networking.

--- devicesetup.sh
+++ devicesetup.sh
@@ -1,6 +1,8 @@
 add_lunch_combo eben-eng
+add_lunch_combo eben-user
 add_lunch_combo gert-eng
-add_lunch_combo gert-userdebug
 add_lunch_combo gert-user
 add_lunch_combo liz-eng
+add_lunch_combo liz-user
 add_lunch_combo pete-eng
+add_lunch_combo pete-user
